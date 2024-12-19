3b705f1c61051cc97611b11afdd250037e41573f
ranjit1024 <ranjitdas2048@gmail.com>
2024-12-19 11:18:40 +0530

    midway

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,9 @@ export function Sidebar() {
   return (
     <div className="bg-slate-800 p-5 h-screen ">
       <div className="logo flex items-center justify-center">
-        <div className="size-10 rounded bg-white"></div>
+        <div className="size-10 rounded bg-white flex overflow-hidden justify-center items-center">
+          <p className="text-[10px] overflow-hidden">Nishyan</p>
+        </div>
 
         <div className="name text-white ml-6">
           <p className="font-medium mb-1">Nishyan</p>
@@ -11,7 +13,7 @@ export function Sidebar() {
           </p>
         </div>
 
-        <div>
+        <div >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -28,6 +30,57 @@ export function Sidebar() {
           </svg>
         </div>
       </div>
+
+      <div className="navigation mt-12">
+        <Menu
+          logo={
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+              />
+            </svg>
+          }
+          name={"Home"}
+        ></Menu>
+
+        <Menu logo={<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
+</svg>
+} name={"Orders"}>
+        
+        </Menu>
+
+
+        <Menu logo={<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z" />
+</svg>
+}  name={"Products"}>
+        </Menu>
+      </div>
+    </div>
+  );
+}
+
+function Menu({ logo, name }) {
+  return (
+    <div className="flex items-center justify-start text-white hover:cursor-pointer hover:bg-slate-700 p-2 rounded-md mb-2">
+      <div className="mr-4">
+        {logo}
+      </div>
+      <div className="text-center font-medium ">
+        {name}
+      </div>
+
+     
     </div>
   );
 }
